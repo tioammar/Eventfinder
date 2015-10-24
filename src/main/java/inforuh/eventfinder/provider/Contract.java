2cccd8ae3177e67995b0019d577b09d0456f1bfb
Aditya Amirullah <adityaamirullah@gmail.com>
2015-10-24 08:24:34 +0800

    adding license

--- src/main/java/inforuh/eventfinder/provider/Contract.java
+++ src/main/java/inforuh/eventfinder/provider/Contract.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Aditya Amirullah. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package inforuh.eventfinder.provider;
 
 import android.content.ContentResolver;
@@ -33,7 +49,7 @@ public class Contract {
         public static final String CATEGORY = "event_category";
         public static final String IMAGE = "event_image";
         public static final String PRICE = "event_price";
-        public static final String LOCATION = "event_location";
+//        public static final String LOCATION = "event_location";
         public static final String LONGITUDE = "event_longitude";
         public static final String LATITUDE = "event_latitude";
 
